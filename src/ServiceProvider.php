eaec14a769356a68f93c4dc98bba2bb9ba4b1971
Tran Phuong Ngoc <phuongngoc@gmail.com>
2016-06-28 08:40:02 +0700

    Set default file path

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -52,7 +52,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     public function register()
     {
         if (config('query_logger.enabled', config('app.debug'))) {
-            $streamHandler = new StreamHandler(config('query_logger.file_path'), Logger::INFO);
+            $streamHandler = new StreamHandler(config('query_logger.file_path', storage_path('logs/query_logger.php')), Logger::INFO);
             $streamHandler->setFormatter(new LineFormatter("%message%;\n"));
             $this->logger = new Logger('sql');
             $this->logger->pushHandler($streamHandler);
