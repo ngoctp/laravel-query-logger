b2810e8279e9de81157570680eeb9ecdd99db880
NgocTP <phuongngoc@gmail.com>
2017-10-04 14:13:50 +0700

    Update service provider to support 5.0 to 5.4

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -16,33 +16,6 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
      * @var \Monolog\Logger $logger
      */
     protected $logger = null;
-    
-    /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->publishes([
-            __DIR__ . '/../config/query_logger.php' => config_path('query_logger.php'),
-        ]);
-        
-        if ($this->logger) {
-            $this->logger->info('----- REQUEST AT '. Carbon::now()->toDateTimeString() .' -----');
-            DB::listen(function (QueryExecuted $query) {
-                $sqlParts = explode('?', $query->sql);
-                $bindings = $query->connection->prepareBindings($query->bindings);
-                $pdo = $query->connection->getPdo();
-                $sql = array_shift($sqlParts);
-                foreach ($bindings as $i => $binding) {
-                    $sql .= $pdo->quote($binding) . $sqlParts[$i];
-                }
-                
-                $this->logger->info($sql);
-            });
-        }
-    }
 
     /**
      * Register the application services.
@@ -51,15 +24,72 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
      */
     public function register()
     {
-        $enabled = config('query_logger.enabled');
-        if (is_null($enabled) ? config('app.debug') : $enabled) {
+        $this->mergeConfigFrom($this->getConfigPath(), 'query_logger');
+
+        if ($this->isLoggerEnabled()) {
             $filePath = config('query_logger.file_path');
             if ($filePath) {
                 $streamHandler = new StreamHandler($filePath, Logger::INFO);
                 $streamHandler->setFormatter(new LineFormatter("%message%;\n"));
-                $this->logger = new Logger('sql');
+                $this->logger = new Logger('query_logger');
                 $this->logger->pushHandler($streamHandler);
             }
         }
+    }
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            $this->getConfigPath() => config_path('query_logger.php'),
+        ]);
+
+        if ($this->logger) {
+            $this->logger->info('-- ------ REQUESTED AT '. Carbon::now()->toDateTimeString() .' ------ --');
+            $this->app['db']->listen(function($query, $bindings = null, $time = null, $name = null) {
+                if ($query instanceof \Illuminate\Database\Events\QueryExecuted) {
+                    $sql = $this->formatQuery($query->sql, $query->bindings, $query->connection);
+                } else {
+                    $sql = $this->formatQuery($query, $bindings, $this->app['db']->connection($name));
+                }
+
+                $this->logger->info($sql);
+            });
+        }
+    }
+
+    private function formatQuery($query, $bindings, $connection)
+    {
+        $sqlParts = explode('?', $query);
+        $bindings = $connection->prepareBindings($bindings);
+        $pdo = $connection->getPdo();
+        $sql = array_shift($sqlParts);
+        foreach ($bindings as $i => $binding) {
+            $sql .= $pdo->quote($binding) . $sqlParts[$i];
+        }
+
+        return $sql;
+    }
+
+    private function isLoggerEnabled()
+    {
+        $enabled = config('query_logger.enabled');
+        if (is_null($enabled)) {
+            $enabled = config('app.debug');
+            if (is_null($enabled)) {
+                $enabled = env('APP_DEBUG', false);
+            }
+        }
+
+        return $enabled;
+    }
+
+    private function getConfigPath()
+    {
+        return __DIR__ . '/../config/query_logger.php';
     }
 }
